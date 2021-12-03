8ea2cc15b3003aeebc88fe21869978d6a463d9e6
NelsonT19 <nelsont408@gmail.com>
2021-12-03 09:13:02 -0100

    up

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 3000;
+
+
+//cliente so tem acesso ao public
+app.use(express.static('./public'))
+
+// pedido em http://localhost:3000\
+app.get('/',(req, res) => {
+ res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+
+app.listen(PORT, function(){
+    console.log(`Server is running on PORT: ` + PORT);
+ });
+
+
